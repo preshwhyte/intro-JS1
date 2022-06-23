6b4a584a2b1fb06682ad27ad796756f8e930484b
unknown <preshwhyte1@gmail.com>
2022-06-23 12:32:06 +0100

    javascript-intro

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let names=" Name: Precious"
+let heigth=" Heigth: 1.7 meters"
+let country="Country: Nigeria"
+let all= names + " " + heigth +" " + country
+
+alert(all)
+
+
+document.write(names," ",heigth, " ",country)
+
+
+console.log(names)
+console.log(heigth)
+console.log(country)
+
+
